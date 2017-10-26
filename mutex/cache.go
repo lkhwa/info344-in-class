b5b3bb9cefd0c9c03ea072de33a47e49b2c89e2b
Leanne Hwa <lkhwa@uw.edu>
2017-10-26 15:09:25 -0700

    end of day 10/26

--- mutex/cache.go
+++ mutex/cache.go
@@ -1,31 +1,64 @@
 package main
 
-import "time"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
 type entry struct {
 	value     string
 	expiresAt time.Time
 }
 
-//Cache is a TTL cache that is safe for concurrent use
+//Cache is a TTL (Time to Live) cache that is safe for concurrent use
+//add something to cache, keep for ___ time, then the entry is removed automaticallys
 type Cache struct {
 	entries map[string]*entry
 	//TODO: protect this for concurrent use!
+	mx sync.RWMutex
 }
 
 //NewCache constructs a new Cache object
 func NewCache() *Cache {
-	return &Cache{
+	c := &Cache{
 		entries: map[string]*entry{},
 	}
+	go c.janitor()
+	return c
 }
 
 //Set adds a key/value to the cache
 func (c *Cache) Set(key string, value string, timeToLive time.Duration) {
+	c.mx.Lock()
+	defer c.mx.Unlock()
 	c.entries[key] = &entry{value, time.Now().Add(timeToLive)}
 }
 
 //Get gets the value associated with a key
 func (c *Cache) Get(key string) (string, bool) {
 	//TODO: implement this
+	c.mx.RLock()         //read
+	defer c.mx.RUnlock() //read
+	entry, found := c.entries[key]
+	if !found {
+		return "", false
+	}
+	return entry.value, true
+}
+
+func (c *Cache) janitor() {
+	for {
+		time.Sleep(time.Second)
+		now := time.Now()
+		c.mx.Lock()
+		fmt.Println("janitor is running")
+		for key, entry := range c.entries {
+			if entry.expiresAt.Before(now) {
+				fmt.Printf("purging key %s\n", key)
+				delete(c.entries, key)
+			}
+		}
+		c.mx.Unlock() //can't use defer because this function is never going to exit, and defer only happens when fnction exits
+	}
 }
--- mutex/main.go
+++ mutex/main.go
@@ -9,8 +9,8 @@ import (
 
 func client(index int, c *Cache) {
 	for {
-		time.Sleep(time.Millisecond * time.Duration(rand.Intn(10)))
-		key := strconv.Itoa(rand.Intn(10))
+		time.Sleep(time.Millisecond * time.Duration(rand.Intn(500)))
+		key := strconv.Itoa(rand.Intn(100000))
 		value := key + " value"
 
 		log.Printf("client %d setting %s=%s", index, key, value)
@@ -24,5 +24,15 @@ func client(index int, c *Cache) {
 func main() {
 	rand.Seed(time.Now().UnixNano())
 	c := NewCache()
+
+	for i := 0; i < 10; i++ {
+		//'go' keyword runs the function on the right of the keyword, but on a new go routine
+		//concurrent running
+		//**when the main function exits, all other go routines exit
+		//so if we don't do anything, all the routines start, but immediately exits
+		go client(i, c)
+	}
+
+	time.Sleep(time.Minute)
 
 }
