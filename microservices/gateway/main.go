68baeaf568ee3189b3d5d46b42c4a693e3fbd136
Leanne Hwa <lkhwa@uw.edu>
2017-11-02 21:00:49 -0700

    node microservices day

--- microservices/gateway/main.go
+++ microservices/gateway/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,6 +11,19 @@ import (
 	"sync"
 )
 
+type User struct {
+	FirstName string `json:"first_name,omitempty"`
+	LastName  string `json:"last_name,omitempty"`
+}
+
+func GetCurrentUser(r *http.Request) *User {
+	//does some magic with our sessions packages
+	return &User{
+		FirstName: "Test",
+		LastName:  "User",
+	}
+}
+
 func NewServiceProxy(addrs []string) *httputil.ReverseProxy {
 	curr := 0
 	//protect against concurrency
@@ -17,6 +31,12 @@ func NewServiceProxy(addrs []string) *httputil.ReverseProxy {
 	return &httputil.ReverseProxy{
 		Director: func(r *http.Request) {
 			//modify the request to indicate remote host
+			user := GetCurrentUser(r)
+			userJSON, err := json.Marshal(user)
+			if err != nil {
+				log.Printf("error marshaling user: %v", err)
+			}
+			r.Header.Add("X-User", string(userJSON))
 			mx.Lock()
 			r.URL.Host = addrs[curr%len(addrs)]
 			curr++
@@ -42,15 +62,20 @@ func main() {
 	//timesvc instances
 	timesvcAddrs := os.Getenv("TIMESVC_ADDRS")
 	splitTimeSvcAddrs := strings.Split(timesvcAddrs, ",")
+
 	hellosvcAddrs := os.Getenv("HELLOSVC_ADDRS")
 	splitHelloSvcAddrs := strings.Split(hellosvcAddrs, ",")
+
+	nodeSvcAddrs := os.Getenv("NODESVC_ADDRS")
+	splitNodeSvcAddrs := strings.Split(nodeSvcAddrs, ",")
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", RootHandler)
 	//TODO: add reverse proxy handler for `/v1/time`
 	mux.Handle("/v1/time", NewServiceProxy(splitTimeSvcAddrs))
 	mux.Handle("/v1/hello", NewServiceProxy(splitHelloSvcAddrs))
-
+	mux.Handle("/v1/users/me/", NewServiceProxy(splitNodeSvcAddrs))
+	mux.Handle("/v1/channels", NewServiceProxy(splitNodeSvcAddrs))
 	log.Printf("server is listening at https://%s...", addr)
 	log.Fatal(http.ListenAndServeTLS(addr, "tls/fullchain.pem", "tls/privkey.pem", mux))
 }
